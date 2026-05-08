7a10f8b3598a48509fea22feaa00b4736a84cc4a
violetalisbeth <00007515@uca.edu.sv>
2026-05-07 23:13:32 -0600

    ✨: adding main

---
+++ labo2/sevenseas/src/main/java/com/ncpas/sevenseas/SevenseasApplication.java
@@ -0,0 +1,13 @@
+package com.ncpas.sevenseas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenseasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenseasApplication.class, args);
+    }
+
+}
