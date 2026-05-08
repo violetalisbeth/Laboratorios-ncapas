6d193e1e99fca4727942411b1e6c2d46067cb886
violetalisbeth <00007515@uca.edu.sv>
2026-05-07 23:08:20 -0600

    ✨: adding pirate repository

---
+++ labo2/sevenseas/src/main/java/com/ncpas/sevenseas/repository/PirateRepository.java
@@ -0,0 +1,11 @@
+package com.ncpas.sevenseas.repository;
+
+import com.ncpas.sevenseas.domain.entity.Pirate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PirateRepository extends JpaRepository<Pirate, UUID> {
+}
