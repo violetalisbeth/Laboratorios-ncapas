feb00c5a99addcfc6a809a4d6307f304767ec419
violetalisbeth <00007515@uca.edu.sv>
2026-05-07 23:16:12 -0600

    📝: adding project support files

---
+++ labo2/sevenseas/src/test/java/com/ncpas/sevenseas/SevenseasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncpas.sevenseas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenseasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
