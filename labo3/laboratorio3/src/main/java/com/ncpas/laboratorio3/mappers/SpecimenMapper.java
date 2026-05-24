e7ac1b65000b90ffda2dd4fd06583c5bfe9ef11c
violetalisbeth <00007515@uca.edu.sv>
2026-05-23 23:04:07 -0600

    ✨: add specimen mapper

---
+++ labo3/laboratorio3/src/main/java/com/ncpas/laboratorio3/mappers/SpecimenMapper.java
@@ -0,0 +1,47 @@
+package com.ncpas.laboratorio3.mappers;
+
+import com.ncpas.laboratorio3.domain.dto.request.CreateSpecimenRequest;
+import com.ncpas.laboratorio3.domain.dto.request.UpdateSpecimenRequest;
+import com.ncpas.laboratorio3.domain.dto.response.specimen.SpecimenResponse;
+import com.ncpas.laboratorio3.domain.entities.Specimen;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class SpecimenMapper {
+
+    public Specimen toEntityCreate(CreateSpecimenRequest request) {
+        return Specimen.builder()
+                .name(request.getName())
+                .region(request.getRegion())
+                .dangerLevel(request.getDangerLevel())
+                .isFriendly(request.getIsFriendly())
+                .build();
+    }
+
+    public Specimen toEntityUpdate(UpdateSpecimenRequest request, UUID id) {
+        return Specimen.builder()
+                .id(id)
+                .name(request.getName())
+                .region(request.getRegion())
+                .dangerLevel(request.getDangerLevel())
+                .isFriendly(request.getIsFriendly())
+                .build();
+    }
+
+    public SpecimenResponse toDto(Specimen specimen) {
+        return SpecimenResponse.builder()
+                .id(specimen.getId())
+                .name(specimen.getName())
+                .region(specimen.getRegion())
+                .dangerLevel(specimen.getDangerLevel())
+                .isFriendly(specimen.getIsFriendly())
+                .build();
+    }
+
+    public Page<SpecimenResponse> toDtoPage(Page<Specimen> specimens) {
+        return specimens.map(this::toDto);
+    }
+}
